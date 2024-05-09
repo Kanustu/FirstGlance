f1c41583f94eaa10e045f04804589afff8d0a700
JDKanius <89230965+Kanustu@users.noreply.github.com>
2024-05-09 11:50:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='FirstGlance',
+    name='first_glance',
     version='1.0.0',
     author='Jordan Kanius',
     author_email='Kaniusjordan@gmail.com',
